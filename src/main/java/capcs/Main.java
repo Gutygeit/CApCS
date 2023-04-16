2722371692bc5104eeb75875109c432ff7816cb4
NathanFallet <contact@nathanfallet.me>
2023-04-16 16:26:43 +0200

    Allow to start daemon with --nogui

--- src/main/java/capcs/Main.java
+++ src/main/java/capcs/Main.java
@@ -1,12 +1,17 @@
 package capcs;
 
+import java.util.Arrays;
+
 import capcs.launcher.DaemonLauncher;
 import capcs.launcher.UILauncher;
 
 public class Main {
 
     public static void main(String[] args) {
-        //new DaemonLauncher();
+        if (Arrays.asList(args).contains("--nogui")) {
+            new DaemonLauncher();
+            return;
+        }
         new UILauncher();
     }
 
